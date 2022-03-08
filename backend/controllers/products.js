0362216810365e6d0ea7e53ec86fb025c0d13556
AbdullahMomani <abdallah.zalmomani@gmail.com>
2022-03-09 01:03:42 +0300

    create getProductType function

--- backend/controllers/products.js
+++ backend/controllers/products.js
@@ -126,12 +126,31 @@ const getProductByName = async (req, res) => {
       .json({ success: true, message: ` product by name`, result: results });
   });
 };
+
+//get products by type //category  //type
+const getProductsByType = (req, res) => {
+  const type = req.query.type;
+  const query = `SELECT * FROM products WHERE is_deleted=0 AND type=?`;
+  const data = [type];
+  connection.query(query, data, (err, results) => {
+    if (err) {
+      res.status(500).json({
+        success: false,
+        message: "server error",
+      });
+    }
+    res
+      .status(200)
+      .json({ success: true, message: ` product by type`, result: results });
+  });
+};
 module.exports = {
     createNewProduct,
     getAllProducts,
     updateProductById,
     deleteProductById,
     getPageProducts,
-    getProductByName
+    getProductByName,
+    getProductsByType
   };
   
