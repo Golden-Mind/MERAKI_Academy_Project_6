2ec9b02b065e841f3d1d5fce5e41b675e4ed31cd
Rubaalnadi <rubaalnadi5@gmail.com>
2022-03-08 21:48:40 +0300

    add role

---
+++ backend/controllers/roles.js
@@ -0,0 +1,23 @@
+const connection = require("../database/db");
+
+const roles = (req, res) => {
+  const role = req.body.role;
+  const query = "INSERT INTO roles (role) VALUES (?)";
+  const data = [role];
+  connection.query(query, data, (err, result) => {
+    if (err) {
+      res.status(500).json({
+        success: false,
+        message: `Server Error`,
+      });
+    } else {
+      res.status(201).json({
+        success: true,
+        message: `Success role created`,
+        role : result,
+        data : data
+      });
+    }
+  });
+};
+module.exports = roles;
