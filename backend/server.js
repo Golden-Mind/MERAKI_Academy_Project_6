ec20aa0f1501cf824261673a2e19708158bc71a9
AbdullahMomani <abdallah.zalmomani@gmail.com>
2022-03-08 22:48:28 +0300

    add product router in server

--- backend/server.js
+++ backend/server.js
@@ -4,12 +4,12 @@ require("dotenv").config();
 const app = express();
 const db = require("./database/db");
 const userRouter = require("./routes/users");
-
+const productRouter = require("./routes/products"); //import products router
 app.use(cors());
 app.use(express.json());
 //================================
 app.use("/user", userRouter);
-
+app.use("/products", productRouter);
 //=================================
 const PORT = 5000;
 
