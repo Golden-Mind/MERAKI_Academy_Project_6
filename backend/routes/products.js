024eaa27103fc55a2e026315476486b36a187a4e
AbdullahMomani <abdallah.zalmomani@gmail.com>
2022-03-08 23:28:56 +0300

    add delete product rout

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -1,11 +1,12 @@
 const express = require("express");
 
 const productsRouter = express.Router();
-const {createNewProduct,getAllProducts,updateProductById,
+const {createNewProduct,getAllProducts,updateProductById,deleteProductById
     
   } = require("../controllers/products");
 
   productsRouter.post("/", createNewProduct);
   productsRouter.get("/", getAllProducts);
   productsRouter.patch("/:id", updateProductById);
+  productsRouter.delete("/:id", deleteProductById);
   module.exports = productsRouter;
