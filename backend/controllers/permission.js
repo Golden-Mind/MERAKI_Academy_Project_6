65db323183a517a2cc78bb5964dad630edb9c5d3
Rubaalnadi <rubaalnadi5@gmail.com>
2022-03-09 01:40:34 +0300

    modify permission

--- backend/controllers/permission.js
+++ backend/controllers/permission.js
@@ -1,11 +1,14 @@
 const connection = require("../database/db");
 
 const permission = (req,res) =>{
-    const permission = req.body.permission;
-    const query = "INSERT INTO permission (permission) VALUES (?)" 
-    const data = [permission]
-    connection.query = (query,data , (err,result) => {
-        if (err) {
+  console.log("ya rabe");
+    const permission = req.body.permissions;
+    const query = "INSERT INTO permission (permission) VALUES (?)";
+    const data = [permission];
+    connection.query(query,data , (err,result) => {
+      console.log("ya rabe");
+
+        if (err) {throw err;
             res.status(500).json({
               success: false,
               message: `Server Error`,
