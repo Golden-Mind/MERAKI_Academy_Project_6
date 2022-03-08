4cc7bcc7f146c31f4323953dcf3d294f5bc25020
AbdullahMomani <abdallah.zalmomani@gmail.com>
2022-03-08 23:15:28 +0300

    creat getAllProduct and update products routes

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -1,9 +1,11 @@
 const express = require("express");
 
 const productsRouter = express.Router();
-const {createNewProduct,
+const {createNewProduct,getAllProducts,updateProductById,
     
   } = require("../controllers/products");
 
   productsRouter.post("/", createNewProduct);
+  productsRouter.get("/", getAllProducts);
+  productsRouter.patch("/:id", updateProductById);
   module.exports = productsRouter;
