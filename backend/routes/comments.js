74b6fd5292bf62ce0b0f832e31b6d96e60c6e90c
AbdullahMomani <abdallah.zalmomani@gmail.com>
2022-03-09 23:08:54 +0300

    create addNewComment route

---
+++ backend/routes/comments.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const commentRouter = express.Router();
+const {
+    addNewComment,
+    getAllComment
+  } = require("../controllers/comments");
+
+
+commentRouter.post("/:id",addNewComment)
+// commentRouter.get("/",getAllComment)
+module.exports = commentRouter;
