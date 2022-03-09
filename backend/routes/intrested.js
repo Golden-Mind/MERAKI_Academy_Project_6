bee3e6e67a608d2a9efcd79c3e9bc4960b0e8e17
AbdullahMomani <abdallah.zalmomani@gmail.com>
2022-03-09 22:03:49 +0300

    create create intersted list end point

--- backend/routes/intrested.js
+++ backend/routes/intrested.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const intrestedRouter = express.Router();
+const {
+    addIntersted,
+  } = require("../controllers/intersted");
 
 
-
-
+intrestedRouter.post("/",addIntersted)
 module.exports = intrestedRouter;
 
