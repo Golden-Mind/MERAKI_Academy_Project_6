71ee0b024a2de6d5a2b3bd6f1e984bcc36131a8a
AbdullahMomani <abdallah.zalmomani@gmail.com>
2022-03-09 22:03:26 +0300

    create create intersted list  function

--- backend/controllers/intersted.js
+++ backend/controllers/intersted.js
@@ -1,6 +1,23 @@
 const connection = require("../database/db");
 
-const intrested = (req,res) => {
+const addIntersted = (req,res) => {
+    const {product_id,user_id}=req.body;
+    const query = `INSERT INTO intersted (product_id,user_id) VALUES (?,?)`;
+const data=[product_id,user_id];
+connection.query(query,data,(err,results)=>{
+    if (err) {
+        res.status(500).json({
+          success: false,
+          message: "Server error",
+        });
+      }
+      res.status(201).json({
+        success: true,
+        message: "product added to intersted list",
+        result: results,
+        data: data,
+      });
+    });
 
 }
-module.exports = intrested;
+module.exports = {addIntersted};
