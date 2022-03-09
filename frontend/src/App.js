5b2e01a0033d0a2800c9c8c4d969f2be9dcfe43a
BeesanGhaith <beesan.ghaith@gmail.com>
2022-03-09 21:52:24 +0300

    App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,15 @@
 import "./App.css";
+import {Routes, Route} from 'react-router-dom'
+import Login from './component/Login/Login'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
     <div className="App">
-      
+      <Routes>
+        <Route path="/" element={<Login/>} />
+      </Routes>
     </div>
   );
 }
