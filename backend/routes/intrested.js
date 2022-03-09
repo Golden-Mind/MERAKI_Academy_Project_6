7826b56577c8e132fa62f43707509a315a8c55fb
AbdullahMomani <abdallah.zalmomani@gmail.com>
2022-03-09 22:36:30 +0300

    create get user intersted list end point

--- backend/routes/intrested.js
+++ backend/routes/intrested.js
@@ -2,9 +2,11 @@ const express = require("express");
 const intrestedRouter = express.Router();
 const {
     addIntersted,
+    getUserIntersted
   } = require("../controllers/intersted");
 
 
 intrestedRouter.post("/",addIntersted)
+intrestedRouter.get("/",getUserIntersted)
 module.exports = intrestedRouter;
 
