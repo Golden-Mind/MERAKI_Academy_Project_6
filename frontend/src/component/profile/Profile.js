a51f28ae898386c98fa1071651604fb7f22a1147
AbdullahMomani <abdallah.zalmomani@gmail.com>
2022-03-10 21:19:49 +0300

    add profile page

---
+++ frontend/src/component/profile/Profile.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom';
+import { Button, Col, Row,Card,Nav, Container,Image,Form,Navbar,NavDropdown,FormControl } from "react-bootstrap";
+export default function Profile() {
+  return (
+    <Container className='row'>
+
+    </Container>
+  )
+}
