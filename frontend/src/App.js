c87d9458419ffd8152ed56fe670fec08be2a416a
Rubaalnadi <rubaalnadi5@gmail.com>
2022-03-10 21:36:39 +0300

    modify app and there stayle

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,14 @@ import "./App.css";
 import {Routes, Route} from 'react-router-dom'
 import Login from './component/Login/Login'
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import Hero from "./component/Hero/Hero";
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Login/>} />
+        <Route path="/login" element={<Login/>} />
+        <Route path="/" element={<Hero />} />
       </Routes>
     </div>
   );
