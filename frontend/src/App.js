63fe16aae03c1a7d45ba7188867fc0b0c07e98e8
Rubaalnadi <rubaalnadi5@gmail.com>
2022-03-13 21:33:58 +0300

    modify app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,7 @@ import Hero from "./component/Hero/Hero";
 import Home from "./component/home/home";
 import Profile from "./component/profile/Profile";
 import Register from "./component/Register/Register";
-
+import Favoraite from "./component/favoraite/Favoraite";
 import { useState } from "react";
 
 function App() {
@@ -15,11 +15,12 @@ function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/login" element={<Login />} />
+        <Route path="/login" element={<Login setUserInfo={setUserInfo} userInfo={userInfo} />} />
         <Route path="/register" element={<Register />} />
         <Route path="/" element={<Hero />} />
         <Route path="/home" element={<Home />} />
         <Route path="/profile" element={<Profile />} />
+        <Route path="/fav" element={<Favoraite userInfo={userInfo} />} />
       </Routes>
     </div>
   );
