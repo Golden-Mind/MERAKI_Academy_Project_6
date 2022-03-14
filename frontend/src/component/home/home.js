fa284fbc6b201327cc41d08336f157bc07c1c9e5
AbdullahMomani <abdallah.zalmomani@gmail.com>
2022-03-14 23:43:15 +0300

    update category option

--- frontend/src/component/home/home.js
+++ frontend/src/component/home/home.js
@@ -133,14 +133,15 @@ console.log(category);
               <NavDropdown
                 title="Category"
                 id="navbarScrollingDropdown"
-                onChange={(e) => {
-                  console.log(e);
+                onClick={(e) => {
+                  
+                  console.log(e.target.innerText)
                   setCategory(e.target.value);
                 }}
                 style={{ backgroundColor: "#13B2A7", color: "white" }}
               >
-                <NavDropdown.Item href="#action3">Car</NavDropdown.Item>
-                <NavDropdown.Item href="#action4">Home</NavDropdown.Item>
+                <NavDropdown.Item>Car</NavDropdown.Item>
+                <NavDropdown.Item >Home</NavDropdown.Item>
                 <NavDropdown.Divider />
                 <NavDropdown.Item href="#action5">
                   Something else here
