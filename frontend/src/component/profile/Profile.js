e16023c28a34bc8fa4c6c56a2740deb710914c49
BeesanGhaith <beesan.ghaith@gmail.com>
2022-03-14 18:19:59 +0300

    update profile.js

--- frontend/src/component/profile/Profile.js
+++ frontend/src/component/profile/Profile.js
@@ -1,22 +1,49 @@
-import React,{useState} from 'react'
-import { useNavigate } from 'react-router-dom';
-import { Button, Col, Row,Card,Nav, Container,Image,Form,Navbar,NavDropdown,FormControl } from "react-bootstrap";
-import Ads from '../ads/Ads';
-import Add from '../ads/Add';
+import React, { useState } from "react";
+import {AiOutlineCloudUpload} from "react-icons/ai"
+import {RiFileList3Line} from "react-icons/ri"
+import {MdOutlineFavoriteBorder} from "react-icons/md"
+import {
+  Nav,
+  Container,
+} from "react-bootstrap";
+import Ads from "../ads/Ads";
+import Add from "../ads/Add";
+import "./profile.css";
+
+
 export default function Profile() {
-    const [ads, setAds] = useState(false);
-    const [add, setAdd] = useState(false);
+  const [ads, setAds] = useState(false);
+  const [add, setAdd] = useState(false);
+
   return (
-    <Container className='d-flex flex-row  mt-5 '  >
-<Nav  className="flex-column gap-2 " style={{fontSize:"3ch"}}>
-  <Nav.Link onClick={()=>{setAdd(true); setAds(false)}}>Add Ads</Nav.Link>
-  <Nav.Link  onClick={()=>{setAds(true); setAdd(false)}}>Your Ads</Nav.Link>
-  <Nav.Link >Favorite</Nav.Link>
-  <Nav.Link >
-    Disabled
-  </Nav.Link>
-</Nav>
-{ads?<Ads/>:add?<Add/>:<></>}
-    </Container>
-  )
+  <>
+  <div style={{display: "flex", flexDirection: "row", gap: "5vw"}}>
+    {/* <Container className="d-flex flex-row mt-5 "> */}
+      <Nav className="Nav">
+        <Nav.Link className="NavLink"
+          onClick={() => {
+            setAdd(true);
+            setAds(false);
+          }}
+          >
+          <AiOutlineCloudUpload style={{marginRight: "0.5vw", fontSize: "2ch"}}/>
+          Add Ads
+        </Nav.Link>
+        <Nav.Link className="NavLink"
+          onClick={() => {
+            setAds(true);
+            setAdd(false);
+          }}
+        >
+          <RiFileList3Line style={{marginRight: "0.5vw"}}/>
+          Your Ads
+        </Nav.Link>
+        <Nav.Link className="NavLink"><MdOutlineFavoriteBorder style={{marginRight: "0.5vw", marginTop:"-1vh"}}/>Favorite</Nav.Link>
+        {/* <Nav.Link className="NavLink">Disabled</Nav.Link> */}
+      </Nav>
+      {ads ? <Ads /> : add ? <Add /> : <></>}
+    {/* </Container> */}
+    </div>
+    </>
+  );
 }
