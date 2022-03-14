9f8ccd85f14ec6f7b6d714b23c52c7270c79d668
Rubaalnadi <rubaalnadi5@gmail.com>
2022-03-14 03:02:56 +0300

    make comments of code demo

--- frontend/src/component/ads/Demo.js
+++ frontend/src/component/ads/Demo.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import { usePosition } from 'use-position';
+// import React from 'react';
+// import { usePosition } from 'use-position';
 
-export const Demo = () => {
-  const watch = true;
-  const {
-    latitude,
-    longitude,
-    timestamp,
-    accuracy,
-    error,
-  } = usePosition(watch);
+// export const Demo = () => {
+//   const watch = true;
+//   const {
+//     latitude,
+//     longitude,
+//     timestamp,
+//     accuracy,
+//     error,
+//   } = usePosition(watch);
 
-  return (
-    <div class="mapouter"><div class="gmap_canvas"><iframe width="600" height="500" id="gmap_canvas" src="https://maps.google.com/maps?q=2880%20Broadway,%20New%20York&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe></div></div>
-  );
-};
+//   return (
+//     <div class="mapouter"><div class="gmap_canvas"><iframe width="600" height="500" id="gmap_canvas" src="https://maps.google.com/maps?q=2880%20Broadway,%20New%20York&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe></div></div>
+//   );
+// };
