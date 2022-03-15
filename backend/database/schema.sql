974eaa1477b6ae736cf7fea7bee20b45b30de051
AbdullahMomani <abdallah.zalmomani@gmail.com>
2022-03-16 02:02:00 +0300

    update schema

--- backend/database/schema.sql
+++ backend/database/schema.sql
@@ -50,7 +50,7 @@ price INT,
 type VARCHAR(255),
 date DATE NOT NULL,
 forr VARCHAR(255),
-phoneNumber INT,
+phoneNumber varchar(255),
 address VARCHAR(255),
 user_id INT,
 FOREIGN KEY (user_id) REFERENCES users(id),
@@ -95,17 +95,17 @@ PRIMARY KEY (id)
 INSERT INTO roles (role) VALUES ("user");
 
 INSERT INTO products (image, productName, description, price, type,date,forr,phoneNumber,address) VALUES ("https://www.motortrend.com/uploads/sites/5/2020/02/2020-Lamborghini-Aventador-SVJ-Roadster-44.jpg
-", "Lambborghini", "model 2020, engine v8, 4.0liter tarbo,700hb, automatic transmission", 510000, "Car",CURRENT_DATE(),'buy',0791156784,'jordan');
-INSERT INTO products (image, productName, description, price, type,date,forr,phoneNumber,address) VALUES ("https://images.netdirector.co.uk/gforces-auto/image/upload/w_343,h_257,dpr_2.0,q_auto,c_fill,f_auto,fl_lossy/auto-client/4f3a94f309517d480173272c101fe224/thumbnail_mobile.jpg", "MG GT", "model 2022, engine v8, 4.0liter tarbo, 700hb, automatic transmission", 20000, "Car",CURRENT_DATE(),'buy',0781750091,'jordan');
+", "Lambborghini", "model 2020, engine v8, 4.0liter tarbo,700hb, automatic transmission", 510000, "Car",CURRENT_DATE(),'Sell','962781750091','jordan');
+INSERT INTO products (image, productName, description, price, type,date,forr,phoneNumber,address) VALUES ("https://images.netdirector.co.uk/gforces-auto/image/upload/w_343,h_257,dpr_2.0,q_auto,c_fill,f_auto,fl_lossy/auto-client/4f3a94f309517d480173272c101fe224/thumbnail_mobile.jpg", "MG GT", "model 2022, engine v8, 4.0liter tarbo, 700hb, automatic transmission", 20000, "Car",CURRENT_DATE(),'Sell',0781750091,'jordan');
 INSERT INTO products (image, productName, description, price, type,date,forr,phoneNumber,address) VALUES ("https://www.motortrend.com/uploads/sites/5/2020/02/2020-Lamborghini-Aventador-SVJ-Roadster-44.jpg
-", "Lambborghini", "model 2020, engine v8, 4.0liter tarbo,700hb, automatic transmission", 510000, "Car",CURRENT_DATE(),'buy',0791156784,'jordan');
-INSERT INTO products (image, productName, description, price, type, date,forr,phoneNumber,address) VALUES ("https://images.netdirector.co.uk/gforces-auto/image/upload/w_343,h_257,dpr_2.0,q_auto,c_fill,f_auto,fl_lossy/auto-client/4f3a94f309517d480173272c101fe224/thumbnail_mobile.jpg", "MG GT", "model 2022, engine v8, 4.0liter tarbo, 700hb, automatic transmission", 20000, "Car",CURRENT_DATE(),'buy',0781750091,'jordan');
+", "Lambborghini", "model 2020, engine v8, 4.0liter tarbo,700hb, automatic transmission", 510000, "Car",CURRENT_DATE(),'Sell','962781750091','jordan');
+INSERT INTO products (image, productName, description, price, type, date,forr,phoneNumber,address) VALUES ("https://images.netdirector.co.uk/gforces-auto/image/upload/w_343,h_257,dpr_2.0,q_auto,c_fill,f_auto,fl_lossy/auto-client/4f3a94f309517d480173272c101fe224/thumbnail_mobile.jpg", "MG GT", "model 2022, engine v8, 4.0liter tarbo, 700hb, automatic transmission", 20000, "Car",CURRENT_DATE(),'Sell',0781750091,'jordan');
 INSERT INTO products (image, productName, description, price, type, date,forr,phoneNumber,address) VALUES ("https://www.motortrend.com/uploads/sites/5/2020/02/2020-Lamborghini-Aventador-SVJ-Roadster-44.jpg
-", "Lambborghini", "model 2020, engine v8, 4.0liter tarbo,700hb, automatic transmission", 510000, "Car",CURRENT_DATE(),'buy',0791156784,'jordan');
-INSERT INTO products (image, productName, description, price, type, date,forr,phoneNumber,address) VALUES ("https://images.netdirector.co.uk/gforces-auto/image/upload/w_343,h_257,dpr_2.0,q_auto,c_fill,f_auto,fl_lossy/auto-client/4f3a94f309517d480173272c101fe224/thumbnail_mobile.jpg", "MG GT", "model 2022, engine v8, 4.0liter tarbo, 700hb, automatic transmission", 20000, "Car",CURRENT_DATE(),'buy',0781750091,'jordan');
+", "Lambborghini", "model 2020, engine v8, 4.0liter tarbo,700hb, automatic transmission", 510000, "Car",CURRENT_DATE(),'Sell','962781750091','jordan');
+INSERT INTO products (image, productName, description, price, type, date,forr,phoneNumber,address) VALUES ("https://images.netdirector.co.uk/gforces-auto/image/upload/w_343,h_257,dpr_2.0,q_auto,c_fill,f_auto,fl_lossy/auto-client/4f3a94f309517d480173272c101fe224/thumbnail_mobile.jpg", "MG GT", "model 2022, engine v8, 4.0liter tarbo, 700hb, automatic transmission", 20000, "Car",CURRENT_DATE(),'Sell',0781750091,'jordan');
 INSERT INTO products (image, productName, description, price, type, date,forr,phoneNumber,address) VALUES ("https://www.motortrend.com/uploads/sites/5/2020/02/2020-Lamborghini-Aventador-SVJ-Roadster-44.jpg
-", "Lambborghini", "model 2020, engine v8, 4.0liter tarbo,700hb, automatic transmission", 510000, "Car",CURRENT_DATE(),'buy',0791156784,'jordan');
-INSERT INTO products (image, productName, description, price, type, date,forr,phoneNumber,address) VALUES ("https://images.netdirector.co.uk/gforces-auto/image/upload/w_343,h_257,dpr_2.0,q_auto,c_fill,f_auto,fl_lossy/auto-client/4f3a94f309517d480173272c101fe224/thumbnail_mobile.jpg", "MG GT", "model 2022, engine v8, 4.0liter tarbo, 700hb, automatic transmission", 20000, "Car",CURRENT_DATE(),'buy',0781750091,'jordan');
+", "Lambborghini", "model 2020, engine v8, 4.0liter tarbo,700hb, automatic transmission", 510000, "Car",CURRENT_DATE(),'Sell','962781750091','jordan');
+INSERT INTO products (image, productName, description, price, type, date,forr,phoneNumber,address) VALUES ("https://images.netdirector.co.uk/gforces-auto/image/upload/w_343,h_257,dpr_2.0,q_auto,c_fill,f_auto,fl_lossy/auto-client/4f3a94f309517d480173272c101fe224/thumbnail_mobile.jpg", "MG GT", "model 2022, engine v8, 4.0liter tarbo, 700hb, automatic transmission", 20000, "Car",CURRENT_DATE(),'Sell',0781750091,'jordan');
 INSERT INTO products (image, productName, description, price, type, date,forr,phoneNumber,address) VALUES ("https://www.motortrend.com/uploads/sites/5/2020/02/2020-Lamborghini-Aventador-SVJ-Roadster-44.jpg
-", "Lambborghini", "model 2020, engine v8, 4.0liter tarbo,700hb, automatic transmission", 510000, "Car",CURRENT_DATE(),'buy',0791156784,'jordan');
-INSERT INTO products (image, productName, description, price, type, date,forr,phoneNumber,address) VALUES ("https://images.netdirector.co.uk/gforces-auto/image/upload/w_343,h_257,dpr_2.0,q_auto,c_fill,f_auto,fl_lossy/auto-client/4f3a94f309517d480173272c101fe224/thumbnail_mobile.jpg", "MG GT", "model 2022, engine v8, 4.0liter tarbo, 700hb, automatic transmission", 20000, "Car",CURRENT_DATE(),'buy',0781750091,'jordan');
+", "Lambborghini", "model 2020, engine v8, 4.0liter tarbo,700hb, automatic transmission", 510000, "Car",CURRENT_DATE(),'Sell','962781750091','jordan');
+INSERT INTO products (image, productName, description, price, type, date,forr,phoneNumber,address) VALUES ("https://images.netdirector.co.uk/gforces-auto/image/upload/w_343,h_257,dpr_2.0,q_auto,c_fill,f_auto,fl_lossy/auto-client/4f3a94f309517d480173272c101fe224/thumbnail_mobile.jpg", "MG GT", "model 2022, engine v8, 4.0liter tarbo, 700hb, automatic transmission", 20000, "Car",CURRENT_DATE(),'Sell',0781750091,'jordan');
