d14dda7614e5f688763249c7e2fe2152b5df73fc
AbdullahMomani <abdallah.zalmomani@gmail.com>
2022-03-16 12:56:44 +0300

    pass prop to profile

--- frontend/src/component/profile/Profile.js
+++ frontend/src/component/profile/Profile.js
@@ -1,49 +1,54 @@
 import React, { useState } from "react";
-import {AiOutlineCloudUpload} from "react-icons/ai"
-import {RiFileList3Line} from "react-icons/ri"
-import {MdOutlineFavoriteBorder} from "react-icons/md"
-import {
-  Nav,
-  Container,
-} from "react-bootstrap";
+import { AiOutlineCloudUpload } from "react-icons/ai";
+import { RiFileList3Line } from "react-icons/ri";
+import { MdOutlineFavoriteBorder } from "react-icons/md";
+import { Nav, Container } from "react-bootstrap";
 import Ads from "../ads/Ads";
 import Add from "../ads/Add";
 import "./profile.css";
 
-
-export default function Profile({ userInfo }) {
+export default function Profile({ userInfo, setId, setHome, setDetails }) {
   const [ads, setAds] = useState(false);
   const [add, setAdd] = useState(true);
-console.log(userInfo);
+  console.log(userInfo);
   return (
-  <>
-  <div style={{display: "flex", flexDirection: "row", gap: "5vw"}}>
-    {/* <Container className="d-flex flex-row mt-5 "> */}
-      <Nav className="Nav">
-        <Nav.Link className="NavLink"
-          onClick={() => {
-            setAdd(true);
-            setAds(false);
-          }}
+    <>
+      <div style={{ display: "flex", flexDirection: "row", gap: "5vw" }}>
+        {/* <Container className="d-flex flex-row mt-5 "> */}
+        <Nav className="Nav">
+          <Nav.Link
+            className="NavLink"
+            onClick={() => {
+              setAdd(true);
+              setAds(false);
+            }}
           >
-          <AiOutlineCloudUpload style={{marginRight: "0.5vw", fontSize: "2ch"}}/>
-          Add Ads
-        </Nav.Link>
-        <Nav.Link className="NavLink"
-          onClick={() => {
-            setAds(true);
-            setAdd(false);
-          }}
-        >
-          <RiFileList3Line style={{marginRight: "0.5vw"}}/>
-          Your Ads
-        </Nav.Link>
-        <Nav.Link className="NavLink"><MdOutlineFavoriteBorder style={{marginRight: "0.5vw", marginTop:"-1vh"}}/>Favorite</Nav.Link>
-        {/* <Nav.Link className="NavLink">Disabled</Nav.Link> */}
-      </Nav>
-      {ads ? <Ads userInfo={userInfo}/> : add ? <Add /> : <></>}
-    {/* </Container> */}
-    </div>
+            <AiOutlineCloudUpload
+              style={{ marginRight: "0.5vw", fontSize: "2ch" }}
+            />
+            Add Ads
+          </Nav.Link>
+          <Nav.Link
+            className="NavLink"
+            onClick={() => {
+              setAds(true);
+              setAdd(false);
+            }}
+          >
+            <RiFileList3Line style={{ marginRight: "0.5vw" }} />
+            Your Ads
+          </Nav.Link>
+          <Nav.Link className="NavLink">
+            <MdOutlineFavoriteBorder
+              style={{ marginRight: "0.5vw", marginTop: "-1vh" }}
+            />
+            Favorite
+          </Nav.Link>
+          {/* <Nav.Link className="NavLink">Disabled</Nav.Link> */}
+        </Nav>
+        {ads ? <Ads userInfo={userInfo} setDetails={setDetails} setHome={setHome} setId={setId} /> : add ? <Add /> : <></>}
+        {/* </Container> */}
+      </div>
     </>
   );
 }
