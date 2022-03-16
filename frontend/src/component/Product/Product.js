4ac3afab2f5459445bf34ef7ddcb2f93195502bb
Rubaalnadi <rubaalnadi5@gmail.com>
2022-03-17 02:05:44 +0300

    modify product to add comment and get it

--- frontend/src/component/Product/Product.js
+++ frontend/src/component/Product/Product.js
@@ -2,10 +2,67 @@ import axios from "axios";
 import React, { useState, useEffect } from "react";
 import { Card, Container, ListGroup, ListGroupItem } from "react-bootstrap";
 import WhatsUp from "./Whatsup";
-
+import { addComments, getComments } from "../../reducer/comments";
+import { useDispatch, useSelector } from "react-redux";
+import Form from 'react-bootstrap/Form';
+import FloatingLabel from 'react-bootstrap/FloatingLabel'
 function Product({ productId, id, userInfo }) {
   const [product, setProduct] = useState();
-  console.log(productId);
+  const [comment, setComment] = useState("");
+  const dispatch = useDispatch();
+  // console.log(productId);
+  console.log(comment);
+  const state = useSelector((state) => {
+    return {
+      product: state.commentesReducer.product,
+      comment: state.commentesReducer.comment,
+    };
+  });
+  //===============================================================
+
+  const addComment = async () => {
+    try {
+      const res = await axios.post(
+        `http://localhost:5000/comment/${id}`,
+        {
+          comment: comment,
+          commenterId: userInfo.id,
+          productId: id,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${state.token}`,
+          },
+        }
+      );
+      console.log(comment);
+      console.log(state.product);
+      console.log(addComments(comment));
+      dispatch(addComments(res.data.results));
+      getComment();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  //=============================================================
+  const getComment = async () => {
+    try {
+      const res = await axios.get(`http://localhost:5000/comment`, {
+        headers: {
+          Authorization: `Bearer ${state.token}`,
+        },
+      });
+      dispatch(getComments(res.data.results));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  //===============================================================
+
+  useEffect(() => {
+    // getAllArticles();
+    getComment();
+  }, []);
   useEffect(() => {
     axios
       .get(`http://localhost:5000/products/product-by/${id}`)
@@ -47,6 +104,17 @@ function Product({ productId, id, userInfo }) {
       ) : (
         <></>
       )}
+    
+      <>
+          <Form.Control
+            as="textarea"
+            placeholder="Leave a comment here"
+            style={{ height: "100px" ,width:"50%","margin-left":"8.5%","margin-top":"3%"}}
+            onChange={(e) => {
+              setComment(e.target.value)
+            }}
+          />
+      </>
     </>
   );
 }
