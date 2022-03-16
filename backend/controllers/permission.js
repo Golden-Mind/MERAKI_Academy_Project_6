78d22535e8b9b1ccf73a30b067ffedf5cfecd8c9
AbdullahMomani <abdallah.zalmomani@gmail.com>
2022-03-16 13:13:24 +0300

    clean the code

--- backend/controllers/permission.js
+++ backend/controllers/permission.js
@@ -1,26 +1,27 @@
 const connection = require("../database/db");
 
-const permission = (req,res) =>{
+const permission = (req, res) => {
   console.log("ya rabe");
-    const permission = req.body.permissions;
-    const query = "INSERT INTO permission (permission) VALUES (?)";
-    const data = [permission];
-    connection.query(query,data , (err,result) => {
-      console.log("ya rabe");
+  const permission = req.body.permissions;
+  const query = "INSERT INTO permission (permission) VALUES (?)";
+  const data = [permission];
+  connection.query(query, data, (err, result) => {
+    console.log("ya rabe");
 
-        if (err) {throw err;
-            res.status(500).json({
-              success: false,
-              message: `Server Error`,
-            });
-          } else {
-            res.status(201).json({
-              success: true,
-              message: `Success role permission`,
-              role : result,
-              data : data
-            });
-          }
-    })
-}
-module.exports = permission
+    if (err) {
+      throw err;
+      res.status(500).json({
+        success: false,
+        message: `Server Error`,
+      });
+    } else {
+      res.status(201).json({
+        success: true,
+        message: `Success role permission`,
+        role: result,
+        data: data,
+      });
+    }
+  });
+};
+module.exports = permission;
